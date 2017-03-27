bae341e9d6e5f66e028e4e745256f89d51e071fc
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-03-27 10:35:33 +0000

    add util class

---
+++ lib/nis/util.rb
@@ -0,0 +1,1 @@
+Dir[File.expand_path('../util/*.rb', __FILE__)].each{|f| require f}
---
+++ lib/nis/util/assignable.rb
@@ -0,0 +1,16 @@
+module Nis::Util
+  module Assignable
+    def initialize(attributes = {})
+      attributes.each do |k, v|
+        send("#{k.to_s}=", v) if respond_to?("#{k.to_s}=")
+      end if attributes
+      yield self if block_given?
+    end
+
+    def to_hash
+      instance_variables.each_with_object({}) do |var, hash|
+        hash[var.to_s.delete("@").to_sym] = instance_variable_get(var)
+      end
+    end
+  end
+end
