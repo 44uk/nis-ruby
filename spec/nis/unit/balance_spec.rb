3d25e7038dfd193b51d5c87b4ae62ba50ba1c8f9
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-04-04 02:16:15 +0000

    update nis/unit/banance

--- spec/nis/unit/balance_spec.rb
+++ spec/nis/unit/balance_spec.rb
@@ -4,6 +4,10 @@ describe Nis::Unit::Balance do
   let(:value) { 123_456_789 }
   let(:balance) { described_class.new(value) }
   let(:other) { described_class.new(value) }
+  let(:small) { described_class.new(1_000_000) }
+  let(:large) { described_class.new(9_000_000) }
+  let(:added) { described_class.new(10_000_000) }
+  let(:subtracted) { described_class.new(8_000_000) }
 
   subject { balance }
 
@@ -13,6 +17,22 @@ describe Nis::Unit::Balance do
 
   describe '#==' do
     it { expect(subject == other).to eq true }
+  end
+
+  describe '#<' do
+    it { expect(small < large).to eq true }
+  end
+
+  describe '#>' do
+    it { expect(large > small).to eq true }
+  end
+
+  describe '#+' do
+    it { expect(large + small).to eq added }
+  end
+
+  describe '#-' do
+    it { expect(large - small).to eq subtracted }
   end
 
   describe '#to_s' do
