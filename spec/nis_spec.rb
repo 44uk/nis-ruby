1b94242f002c8abd6caf6ab3a65f571a20a3327c
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-04-10 23:45:54 +0900

    fixup local/* specs

--- spec/nis_spec.rb
+++ spec/nis_spec.rb
@@ -158,6 +158,51 @@ describe Nis do
     )).to be_a Array }
   end
 
+  describe '#local_account_transfers_incoming' do
+    let(:page) do
+      Nis::Struct::AccountPrivateKeyTransactionsPage.new(
+        value: '68e4f79f886927de698df4f857de2aada41ccca6617e56bb0d61623b35b08cc0',
+        hash: '44e4968e5aa35fe182d4def5958e23cf941c4bf809364afb4431ebbf6a18c039',
+        id: 12345
+      )
+    end
+    it { expect(subject.local_account_transfers_incoming(
+      page: page
+    )).to be_a Array }
+  end
+
+  describe '#local_account_transfers_outgoing' do
+    let(:page) do
+      Nis::Struct::AccountPrivateKeyTransactionsPage.new(
+        value: '68e4f79f886927de698df4f857de2aada41ccca6617e56bb0d61623b35b08cc0',
+        hash: '44e4968e5aa35fe182d4def5958e23cf941c4bf809364afb4431ebbf6a18c039',
+        id: 12345
+      )
+    end
+    it { expect(subject.local_account_transfers_outgoing(
+      page: page
+    )).to be_a Array }
+  end
+
+  describe '#local_account_transfers_all' do
+    let(:page) do
+      Nis::Struct::AccountPrivateKeyTransactionsPage.new(
+        value: '68e4f79f886927de698df4f857de2aada41ccca6617e56bb0d61623b35b08cc0',
+        hash: '44e4968e5aa35fe182d4def5958e23cf941c4bf809364afb4431ebbf6a18c039',
+        id: 12345
+      )
+    end
+    it { expect(subject.local_account_transfers_all(
+      page: page
+    )).to be_a Array }
+  end
+
+  describe '#local_chain_blocks_after' do
+    it { expect(subject.local_chain_blocks_after(
+      block_height: 2649
+    )).to be_a Array }
+  end
+
   describe '#debug_connections_incoming' do
     it { expect(subject.debug_connections_incoming)
       .to be_a Nis::Struct::AuditCollection }
