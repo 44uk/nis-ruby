4537d95b63a6087f2cfa85d42290c004ddeaf320
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-04-12 07:58:17 +0900

    add Explorer* structs

--- lib/nis/struct/explorer_block_view_model.rb
+++ lib/nis/struct/explorer_block_view_model.rb
@@ -1,14 +1,16 @@
 class Nis::Struct
-  # @attr [Array <Nis::Struct::Transaction>] txes
-  # @attr [String] block
+  # @attr [Array <Nis::Struct::ExplorerTransferViewModel>] txes
+  # @attr [Nis::Struct::Block] block
   # @attr [String] hash
+  # @attr [String] difficulty
   # @see http://bob.nem.ninja/docs/#explorerBlockViewModel
   class ExplorerBlockViewModel
     include Nis::Util::Assignable
-    attr_accessor :txes, :block, :hash
+    attr_accessor :txes, :block, :hash, :difficulty
 
     def self.build(attrs)
-      attrs[:txes] = attrs[:txes].map { |t| Transaction.new(t) }
+      attrs[:txes] = attrs[:txes].map { |tx| ExplorerTransferViewModel.build(tx) }
+      attrs[:block] = Block.build(attrs[:block])
       new(attrs)
     end
   end
