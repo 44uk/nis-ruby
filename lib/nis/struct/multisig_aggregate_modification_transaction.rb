8a3769041e4043fabe55d8c03ca9d0a2061ddc5f
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-06-13 22:26:42 +0900

    add multisig aggregate modification transaction

---
+++ lib/nis/struct/multisig_aggregate_modification_transaction.rb
@@ -0,0 +1,60 @@
+class Nis::Struct
+  # @attr [Integer] timeStamp
+  # @attr [String]  signature
+  # @attr [Integer] fee
+  # @attr [Integer] type
+  # @attr [Integer] deadline
+  # @attr [Integer] version
+  # @attr [String] signer
+  # @attr [Array <Nis::Struct::MultisigCosignatoryModification>] modifications
+  # @attr [Hash] minCosignatories
+  # @see http://bob.nem.ninja/docs/#multisigAggregateModificationTransaction
+  class MultisigAggregateModificationTransaction
+    include Nis::Util::Assignable
+    attr_accessor :timeStamp, :signature, :fee, :type, :deadline, :version, :signer,
+                  :modifications, :minCosignatories
+
+    alias timestamp timeStamp
+    alias timestamp= timeStamp=
+    alias min_cosignatories minCosignatories
+    alias min_cosignatories= minCosignatories=
+
+    TYPE = 0x1001 # 4097 (multisig aggregate modification transfer transaction)
+    FEE  = 16_000_000
+
+    def self.build(attrs)
+      new(attrs)
+    end
+
+    # @return [Integer]
+    def _version
+      (0xFFFFFFF0 & @version)
+    end
+
+    # @return [Boolean]
+    def testnet?
+      (0x0000000F & @version) == Nis::Util::TESTNET
+    end
+
+    # @return [Boolean]
+    def mainnet?
+      (0x0000000F & @version) == Nis::Util::MAINNET
+    end
+
+    def fee
+      @fee ||= calculate_fee
+    end
+
+    alias to_hash_old to_hash
+
+    def to_hash
+      fee
+      to_hash_old
+    end
+
+    # @return [Integer]
+    def calculate_fee
+      FEE
+    end
+  end
+end
