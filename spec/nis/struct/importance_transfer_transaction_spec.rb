42ac5099163ab1fd4b309e347ba266614c106f19
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-06-14 05:31:57 +0000

    update test struct/importance_transfer_transaction

---
+++ spec/nis/struct/importance_transfer_transaction_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Nis::Struct::ImportanceTransferTransaction do
+  let(:struct) do
+    described_class.new
+  end
+
+  subject { struct }
+
+  describe '#type' do
+    it { expect(subject.type).to eq 0x0801 }
+  end
+
+  describe '#fee' do
+    it { expect(subject.fee).to eq 6_000_000 }
+  end
+end
