b9a68a98c730ff41a3f888c2ec8f5c22ba88f507
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-07-14 04:50:11 +0000

    update examples/*.rb

--- examples/namespace.rb
+++ examples/namespace.rb
@@ -1,12 +1,9 @@
 require 'nis'
-hr = '-' * 64
 
 nis = Nis.new
 
 ns_mosaics = nis.namespace_mosaic_definition_page(namespace: 'alice.misc')
 puts ns_mosaics.first.to_hash
-puts hr
 
 ns_root = nis.namespace_root_page
 puts ns_root.first.to_hash
-puts hr
