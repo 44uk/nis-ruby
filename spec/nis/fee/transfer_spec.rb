131680e79d311b9bcb5f92e732f4be9051d0405e
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-08-31 08:25:55 +0900

    add spec mosaic transfer

--- spec/nis/fee/transfer_spec.rb
+++ spec/nis/fee/transfer_spec.rb
@@ -38,6 +38,114 @@ describe Nis::Fee::Transfer do
     it { is_expected.to eq 100_000 }
   end
 
+  context 'small business mosaic transfer' do
+    let(:mo_def) do
+      Nis::Struct::MosaicDefinition.new(
+        properties: Nis::Struct::MosaicProperties.new(
+          divisibility: 0,
+          initialSupply: 10_000,
+          supplyMutable: false,
+          transferable: true
+        )
+      )
+    end
+
+    context '1ns:mos, no message' do
+      let(:mosaics) { [ Nis::Struct::MosaicAttachment.new(mo_def, 1_000_000) ] }
+      it { is_expected.to eq 50_000 }
+    end
+  end
+
+  context 'nem:xem transfer' do
+    let(:mo_def) do
+      Nis::Struct::MosaicDefinition.new(
+        properties: Nis::Struct::MosaicProperties.new(
+          divisibility: 6,
+          initialSupply: 8_999_999_999,
+          supplyMutable: false,
+          transferable: true
+        )
+      )
+    end
+
+    context '1nem:xem, no message' do
+      let(:mosaics) { [ Nis::Struct::MosaicAttachment.new(mo_def, 1_000_000) ] }
+      it { is_expected.to eq 50_000 }
+    end
+
+    context '19,999nem:xem, no message' do
+      let(:mosaics) { [ Nis::Struct::MosaicAttachment.new(mo_def, 19_999_000_000) ] }
+      it { is_expected.to eq 50_000 }
+    end
+
+    context '20,000nem:xem, no message' do
+      let(:mosaics) { [ Nis::Struct::MosaicAttachment.new(mo_def, 20_000_000_000) ] }
+      it { is_expected.to eq 100_000 }
+    end
+  end
+
+  context 'ns:mos transfer' do
+    let(:mo_def) do
+      Nis::Struct::MosaicDefinition.new(
+        properties: Nis::Struct::MosaicProperties.new(
+          divisibility: 3,
+          initialSupply: 100_000_000,
+          supplyMutable: false,
+          transferable: true
+        )
+      )
+    end
+
+    context '1,222.212ns:mos, no message' do
+      let(:mosaics) { [ Nis::Struct::MosaicAttachment.new(mo_def, 1_222_212) ] }
+      it { is_expected.to eq 100_000 }
+    end
+
+    context '1,333.323ns:mos, no message' do
+      let(:mosaics) { [ Nis::Struct::MosaicAttachment.new(mo_def, 1_333_323) ] }
+      it { is_expected.to eq 150_000 }
+    end
+  end
+
+  context 'nem:xem and ns:mos transfer' do
+    let(:mo_def1) do
+      Nis::Struct::MosaicDefinition.new(
+        properties: Nis::Struct::MosaicProperties.new(
+          divisibility: 6,
+          initialSupply: 8_999_999_999,
+          supplyMutable: false,
+          transferable: true
+        )
+      )
+    end
+    let(:mo_def2) do
+      Nis::Struct::MosaicDefinition.new(
+        properties: Nis::Struct::MosaicProperties.new(
+          divisibility: 3,
+          initialSupply: 100_000_000,
+          supplyMutable: false,
+          transferable: true
+        )
+      )
+    end
+
+    context '1ns:mos and 1nem:xem, no message' do
+      let(:mosaics) {
+        [ Nis::Struct::MosaicAttachment.new(mo_def1, 1_000_000),
+          Nis::Struct::MosaicAttachment.new(mo_def2, 1_000) ]
+      }
+      it { is_expected.to eq 100_000 }
+    end
+
+    context '1,222.212ns:mos and 19,999.000001nem:xem, no message' do
+      let(:mosaics) {
+        [ Nis::Struct::MosaicAttachment.new(mo_def1, 19_999_000_001),
+          Nis::Struct::MosaicAttachment.new(mo_def2, 1_222_212) ]
+      }
+      it { is_expected.to eq 200_000 }
+    end
+  end
+
   context 'on mainnet' do
     let(:network) { :maitnet }
 
