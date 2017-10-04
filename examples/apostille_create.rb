31567e39f70cee46eb2852fc0b96c1cdd457cc21
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-10-04 07:36:24 +0000

    fixup examples/apostille_create.rb

--- examples/apostille_create.rb
+++ examples/apostille_create.rb
@@ -9,6 +9,7 @@ FIXTURES_PATH = File.expand_path('../../spec/fixtures', __FILE__)
 A_PRIVATE_KEY = '4ce5c8f9fce571db0d9ac1adf00b8d3ba0f078ed40835fd3d730a2f24b834214'
 
 kp = Nis::Keypair.new(A_PRIVATE_KEY)
+
 file = File.open("#{FIXTURES_PATH}/nemLogoV2.png")
 ap = Nis::Apostille.new(kp, file, :sha1,
   multisig: false,
@@ -17,7 +18,6 @@ ap = Nis::Apostille.new(kp, file, :sha1,
 )
 tx = ap.transaction
 p "Fee: #{tx.fee.to_i}"
-p tx.inspect
 
 req = Nis::Request::Announce.new(tx, kp)
 res = nis.transaction_announce(req)
