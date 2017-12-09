39e888fb3dfbe442e4d7f0176bd35bdd0ba5b2de
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-12-10 02:03:30 +0900

    remove useless sample

--- examples/account/get.rb
+++ examples/account/get.rb
@@ -6,21 +6,12 @@ A_PUBLIC_KEY = 'cc63b4dcdec745417043c3fa0992ec3a1695461a26d90264744648abbd5caa0d
 
 nis = Nis.new(host: '23.228.67.85')
 
+# /account/get?address={address}
+#   -> account_get address: {address}
 p nis.account_get(address: A_ADDRESS)
 p nis.account_get_from_public_key(public_key: A_PUBLIC_KEY)
 
-p nis.account_get_forwarded(address: A_ADDRESS)
-p nis.account_get_forwarded_from_public_key(public_key: A_PUBLIC_KEY)
-
-# /account/get?address={address}
-#   -> account_get address: {address}
-# Passing parameters by keyword arguments.
-account_meta_pair = nis.account_get(address: keypair.address)
-account = account_meta_pair[:account]
-p account.balance
-
 # /account/get/from-public-key?public-key={key}
 #   -> account_get_public_key public_key: {key}
-account_meta_pair = nis.account_get_from_public_key(public_key: keypair.public_key)
-account = account_meta_pair.account
-p account.address
+p nis.account_get_forwarded(address: A_ADDRESS)
+p nis.account_get_forwarded_from_public_key(public_key: A_PUBLIC_KEY)
